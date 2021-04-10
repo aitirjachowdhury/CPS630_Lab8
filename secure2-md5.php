a7c6c114c99c438654a6027eca01acfddfbeb983
Felicia Levina <felicialevina10@gmail.com>
2021-04-11 00:52:05 +0800

    secure

--- secure2-md5.php
+++ secure2-md5.php
@@ -1,9 +1,10 @@
+<?php
 
 //Second approach to storing passwords (using Secure Hash algorithm by MD5)
 
 //Insert the user with the password being hashed by MD5 first.
 function insertUser($username,$password){
-  $pdo = new PDO(DBCONN_STRING,DBUSERNAME,DBPASS);
+  $pdo = new PDO("mysql:host=localhost;dbname=lab8", 'root', '');
   $sql = "INSERT INTO  Users(Username,Password)
          VALUES(?,?)";
   $smt = $pdo->prepare($sql);
@@ -12,7 +13,7 @@ function insertUser($username,$password){
 
 //Check if the credentials match a user in the system with MD5 hash
 function validateUser($username,$password){
-  $pdo = new PDO(DBCONN_STRING,DBUSERNAME,DBPASS);
+  $pdo = new PDO("mysql:host=localhost;dbname=lab8", 'root', '');
   $sql = "SELECT  UserID FROM Users WHERE  Username=? AND
         Password=?";
   $smt = $pdo->prepare($sql);
@@ -23,9 +24,16 @@ function validateUser($username,$password){
   return false; //record not found matching credentials, return false
 }
 
+insertUser('Smith','password1');
+insertUser('David', 'password2');
+echo validateUser('Smith', 'password1');
+echo "<br>";
+echo validateUser('David', 'password2');
 
+?>
+<!--
 ----------------------------------------------------------------------------------------------------------------------------
-Password Storage: Secure Hash Method --> MD5("password"); 
+Password Storage: Secure Hash Method  MD5("password"); 
 
 user1 password: "password"
 user2 password: "password"
@@ -33,3 +41,4 @@ user2 password: "password"
 User Id (int) 		User Name (varchar)		Password (varchar)
 1			Smith				gastiweiwcnbmxncxcvuyw28e29es
 2			David				gastiweiwcnbmxncxcvuyw28e29es
+-->
--- secure3-salted.php
+++ secure3-salted.php
@@ -1,13 +1,14 @@
+<?php
 
 //Third approach: An Authentication system using salted passwords
 
 function generateRandomSalt(){
-  return base64_encode(mcrypt_create_iv(12), MCRYPT_DEV_URANDOM));
+  return base64_encode(mcrypt_create_iv(12, MCRYPT_DEV_RANDOM));
 }
 // Insert the user with the password salt generated, stored, and
 // password hashed
 function insertUser($username,$password){
-  $pdo = new PDO(DBCONN_STRING,DBUSERNAME,DBPASS);
+  $pdo = new PDO("mysql:host=localhost;dbname=lab8", 'root', '');
   $salt = generateRandomSalt();
   $sql = "INSERT INTO Users(Username,Password,Salt)
            VALUES(?,?,?)";
@@ -15,8 +16,37 @@ function insertUser($username,$password){
   $smt->execute(array($username,md5($password.$salt),$salt));
 }
 
+/*
+//Check if the credentials match a user in the system with MD5 Salt hash
+function validateUser($username,$password){
+  $pdo = new PDO("mysql:host=localhost;dbname=lab8", 'root', '');
+  $sql = "SELECT  Salt FROM Users WHERE  Username=?";
+  $smt = $pdo->prepare($sql);
+
+  while ($row = $stmt->fetch()) {
+
+    $pdo = new PDO("mysql:host=localhost;dbname=lab8", 'root', '');
+    $sql = "SELECT  UserID FROM Users WHERE  Username=? AND
+          Password=?";
+
+$smt->execute(array($username,md5($password.$row['salt']))); //execute the query
+if($smt->rowCount()){
+  return true; //record found, return true.
+}
+return false; //record not found matching credentials, return false
+}
+}
+*/
+insertUser('Smith','password1');
+//insertUser('David', 'password2');
+//echo validateUser('Smith', 'password1');
+//echo "<br>";
+//echo validateUser('David', 'password2');
 
 
+?>
+
+<!--
 ----------------------------------------------------------------------------------------------------------------------------
 Password Storage: Salted passwords encrypted with a one-way Hash
 
@@ -26,3 +56,4 @@ user2: MD5("password54321a");
 User Id (int) 		User Name (varchar)	Salt		Password (varchar)
 1			Smith			12345a	 	jsdkskdksdkqtywtqywcbaueiuwe64
 2			David			54321a	 	flkfdlkfldkfldflkgle9wenbxvnzxuquwe
+-->
