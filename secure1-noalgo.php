6b13b19884a75fc28baa5e3af605a592c841827d
Felicia Levina <felicialevina10@gmail.com>
2021-04-10 20:35:58 +0800

    Update secure1

--- secure1-noalgo.php
+++ secure1-noalgo.php
@@ -1,18 +1,19 @@
+<?php
 
 // First approach to storing passwords (very insecure)
 
 //Insert the user with the password
 function insertUser($username,$password){
-  $pdo = new PDO(DBCONN_STRING,DBUSERNAME,DBPASS);
+  $pdo = new PDO("mysql:host=localhost;dbname=lab8", 'root', '');
   $sql = "INSERT INTO Users(Username,Password) 
-          VALUES('?,?')");
+          VALUES(?,?)";
   $smt = $pdo->prepare($sql);
   $smt->execute(array($username,$password)); //execute the query
 }
 
 //Check if the credentials match a user in the system
 function validateUser($username,$password){
-  $pdo = new PDO(DBCONN_STRING,DBUSERNAME,DBPASS);
+  $pdo = new PDO("mysql:host=localhost;dbname=lab8", 'root', '');
   $sql = "SELECT UserID FROM Users WHERE Username=? AND
           Password=?";
   $smt = $pdo->prepare($sql);
@@ -23,6 +24,13 @@ function validateUser($username,$password){
   return false; //record not found matching credentials, return false
 }
 
+insertUser('Smith','password1');
+insertUser('David', 'password2');
+echo validateUser('Smith', 'password1');
+echo "<br>";
+echo validateUser('David', 'password2');
+?>
+<!--
 ----------------------------------------------------------------------------------------------------------------------------
 Password Storage: Plain Text, Insecure Method, No Confidentiality, No Integrity
 
@@ -30,4 +38,4 @@ User Id (int) 		User Name (varchar)		Password (varchar)
 1			Smith				password1
 2			David				password2
 
-
+-->
